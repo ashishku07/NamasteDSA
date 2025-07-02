d731e1c3a017f1e2853445e2a338f444f071063a
Ashish Kumar Gupta <104450270+ashishku07@users.noreply.github.com>
2025-07-02 19:04:56 +0530

    Create DSAday1.js

---
+++ DSAday1.js
@@ -0,0 +1,14 @@
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr[2])
+
+// let arr = [1, 2, 3, 4, 5,[1,2,3]];
+// console.log(arr[5][2])
+
+
+
+// let obj = {
+//     a: 1,
+//     name: "Ashish"
+// }
+
+// console.log(obj.a)
